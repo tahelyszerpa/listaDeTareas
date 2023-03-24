136c0e1bb8ec7362f6e0fab3a6d0bb90a08011be
Tahelys Zerpa <tahelyszerpa@gmail.com>
2023-03-24 10:50:36 -0300

    se sube la lista de tarea

---
+++ src/js/main.js
@@ -0,0 +1,111 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
+const itemLi = document.querySelector("ul li");
+const total = document.querySelector("#total");
+const completadas = document.querySelector("#completadas");
+const fecha = document.querySelector("#fecha");
+
+const fechaActual = new Date();
+fecha.innerHTML = fechaActual.toLocaleDateString('es-AR', { weekday: 'long', month: 'short', day: 'numeric' })
+let cont = 0
+
+let tareas = []
+addBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const text = input.value;
+    const ObjTarea = {
+        id: Date.now(),
+        tarea: text,
+        estado: false
+    }
+
+    tareas = [...tareas, ObjTarea];
+    console.log(tareas);
+
+    if (text !== "") {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+
+        p.textContent = text;
+
+        li.appendChild(p);
+        ul.appendChild(li);
+        li.appendChild(addDeleteBtn());
+        li.appendChild(addCheckBtn());
+
+        input.value = "";
+        empty.style.display = "none";
+
+    }
+    mostrarHTML();
+});
+
+function mostrarHTML() {
+    tareas.forEach((item) => {
+        let totalTareas = tareas.length;
+
+        let tareasCompletas = cont
+        console.log('El valor es: ', tareasCompletas)
+
+
+        total.textContent = `Total tareas: ${totalTareas}`;
+
+    }
+    )
+}
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x"
+    deleteBtn.className = "btn-delete"
+
+    deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+        if (tareas.tarea == item.textContent) {
+            tareas.forEach(function (item) {
+                tareas.filter((a) => a !== item.textContent)
+
+            })
+        }
+
+        const items = document.querySelectorAll("li");
+
+        if (items.length === 0) {
+            empty.style.display = "block";
+        }
+
+    })
+    return deleteBtn;
+};
+
+
+function addCheckBtn() {
+
+    const checkBtn = document.createElement("button");
+    checkBtn.textContent = "/";
+    checkBtn.className = "btn-check";
+
+    checkBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        const listo = item.firstElementChild
+        console.log()
+        if (tareas.tarea == item.textContent) {
+            tareas.forEach(function (item) {
+                item.estado = true
+
+            })
+        }
+        cont = cont + 1;
+
+        listo.setAttribute("style", "text-decoration:line-through;");
+        completadas.textContent = `Tareas Completadas: ${cont}`;
+
+    });
+    return checkBtn;
+};
+
